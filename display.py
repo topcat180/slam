29192d4053d300916caec62616dc81554b0c6835
topcat180 <tom.canny@gmail.com>
2020-09-07 17:58:28 -0500

    adding display.py

---
+++ display.py
@@ -0,0 +1,23 @@
+import sdl2
+import sdl2.ext
+
+class Display(object):
+	def __init__(self, W, H):
+		sdl2.ext.init()
+
+		self.W, self.H = W, H
+		self.window = sdl2.ext.Window("Hello World!", size=(W, H), position = ((W*-1-200),200))
+		self.window.show()
+		
+	def paint(self, img):
+		# junk
+		events = sdl2.ext.get_events()
+		for event in events:
+			if event.type == sdl2.SDL_QUIT:
+				exit(0)
+		# draw
+		surf = sdl2.ext.pixels2d(self.window.get_surface())
+		surf[:] = img.swapaxes(0,1)[:, :, 0]
+		
+		# blit
+		self.window.refresh()
